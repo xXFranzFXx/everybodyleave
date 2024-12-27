981c42b0955dbb000e669098ab624ef63f4f0e45
xXFranzFXx <franz.fernando@testpro.io>
2024-12-26 20:36:23 -0800

    updates validation schema

---
+++ src/form-components/FormAcceptTerms.jsx
@@ -0,0 +1,53 @@
+import {
+    Paper,
+    Box,
+    Grid,
+    TextField,
+    Typography,
+    FormControlLabel,
+    Checkbox,
+    Button
+  } from '@material-ui/core';
+
+import { Controller, useForm } from 'react-hook-form';
+const FormAcceptTerms = () => {  
+    const {
+        register,
+        control,
+       
+        formState: { errors }
+      } = useForm({
+       // resolver: yupResolver(validationSchema)
+      });
+      return (
+            <>
+                <FormControlLabel
+                control={
+                <Controller
+                    control={control}
+                    name="acceptTerms"
+                    defaultValue="false"
+                    inputRef={register()}
+                    render={({ field: { onChange } }) => (
+                    <Checkbox
+                        color="primary"
+                        onChange={e => onChange(e.target.checked)}
+                    />
+                    )}
+                />
+                }
+                label={
+                <Typography color={errors.acceptTerms ? 'error' : 'inherit'}>
+                    I have read and agree to the Terms *
+                </Typography>
+                }
+                />
+                <br />
+                <Typography variant="inherit" color="textSecondary">
+                {errors.acceptTerms
+                ? '(' + errors.acceptTerms.message + ')'
+                : ''}
+                </Typography>
+            </>
+      )
+}
--- src/form/MainForm.jsx
+++ src/form/MainForm.jsx
@@ -5,8 +5,11 @@ import { useForm } from "react-hook-form";
 import { FormInputDate } from '../form-components/FormInputDate';
 import { FormInputTel } from '../form-components/FormInputTel';
 import { FormInputTime } from '../form-components/FormInputTime';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 const MainForm = () => {
+    
     const { state } = useSocketContext();
     const methods = useForm({ defaultValues: state || "" });
     const { handleSubmit, getValues, reset, control, setValue } = methods;
