cb54b33124b829f91721340150f5dc4d779ccd5c
xXFranzFXx <franz.fernando@testpro.io>
2024-12-30 17:49:22 -0800

    update MainForm

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { ThemeProvider } from '@mui/material/node/styles';
 import { CssBaseline } from '@mui/material/node';
 import { createTheme } from '@mui/material/node/styles'; 
+import MainForm from './form/MainForm';
 
 function App() {
   const theme = createTheme({
@@ -29,22 +30,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
             <CssBaseline/>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+              <MainForm/>
     </ThemeProvider>
   );
 }
--- src/form/MainForm.jsx
+++ src/form/MainForm.jsx
@@ -23,3 +23,4 @@ const MainForm = () => {
     )
 
 }
+export default MainForm;
