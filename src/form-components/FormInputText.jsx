5bbf9472e54843a0cca91c6a453b2123a9c75e53
xXFranzFXx <franz.fernando@testpro.io>
2025-01-05 14:49:51 -0800

    Update FormInputText.jsx

--- src/form-components/FormInputText.jsx
+++ src/form-components/FormInputText.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Controller } from "react-hook-form";
 import TextField from "@mui/material/TextField";
-export const FormInputText = ({ name, control, label }) => {
+import { Typography } from "@mui/material/node";
+
+export const FormInputText = ({ name, control, label, errors }) => {
     return (
+        <>
         <Controller 
             name={name} 
             control={control} 
-            render={({ field: { onChange, value }, fieldState: { error }, formState, }) => (
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
                 <TextField helperText={error ? error.message : null} error={!!error} onChange={onChange} value={value} fullWidth label={label} variant="outlined"/>
             )} 
             rules={{ required: { value: true, message: 'This field is required' } }}
         />
+        <Typography variant="inherit" color="textSecondary">
+        {errors.name?.message}
+      </Typography>
+      </>
     );
 };
