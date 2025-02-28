f21e3b54ef1c87f196a27cb81b730960ef267d36
xXFranzFXx <franz.fernando@testpro.io>
2025-02-28 06:46:25 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <SocketProvider> */}
+   <SocketProvider>
       <App />
-    {/* </SocketProvider> */}
+    </SocketProvider> 
   </React.StrictMode>
 );
 
