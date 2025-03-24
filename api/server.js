9767ffd1de838913f7eb339b53bce5c282754ca4
xXFranzFXx <franz.fernando@testpro.io>
2025-03-23 21:10:20 -0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -15,15 +15,16 @@ const corsOptions = {
   'Access-Control-Allow-Origin': ["*"],
   methods: ["OPTIONS", "GET", "POST"]
 };
+
 app.use(cors());
 app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.json());
+
 const staticPath = path.join(__dirname, "build");
-
-
 const PORT = process.env.PORT || 4000;
+
 const socketIO = require("socket.io")(http, {
   connectionStateRecovery: {
     // the backup duration of the sessions and the packets
@@ -31,15 +32,15 @@ const socketIO = require("socket.io")(http, {
     // whether to skip middlewares upon successful recovery
     skipMiddlewares: true
   },
-  allowEIO3: true,
-  transports: ['websocket', 'polling', 'flashSocket'],
-  cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001"],
-    'Access-Control-Allow-Origin': ["http://localhost:3000", "http://localhost:3001"],
-    methods: ["OPTIONS", "GET", "POST"]
-  },
-  pingTimeout: 60000,
-  maxHttpBufferSize: 2e8
+    allowEIO3: true,
+    transports: ['websocket', 'polling', 'flashSocket'],
+    cors: {
+      origin: ["http://localhost:3000", "http://localhost:3001"],
+      'Access-Control-Allow-Origin': ["http://localhost:3000", "http://localhost:3001"],
+      methods: ["OPTIONS", "GET", "POST"]
+    },
+    pingTimeout: 60000,
+    maxHttpBufferSize: 2e8
 });
 
 socketIO.on("connection", socket => {
@@ -47,23 +48,26 @@ socketIO.on("connection", socket => {
     console.log(process.env.USER);
     console.log(process.env.NODE_ENV);
    
-    socket.on('notificationsToSocket', async () => {
-      const { notifications } = getNotifications();
-      socket.emit('notifiactions', notifications);
-    });
+    // socket.on('notificationsToSocket', async () => {
+    //   const { notifications } = await getNotifications();
+    //   socket.emit('notifiactions', notifications);
+    // });
     socket.on('sendTwilioSms', async (data) => {
       const { sid } = await cronJob(data);
       socket.emit('twilioSms', sid);
     })
-    socket.on("disconnect", () => {
-      socket.disconnect();
-      console.log("🔥: A user disconnected");
-    });
+   
   socket.on('send46ElksSms', async(data, param) => {
     const { phone } = data;
     const { smsSent } = await sms46Elks(phone, param);
     socket.emit('46ElksSms', smsSent);
   })
+
+  socket.on("disconnect", () => {
+    socket.disconnect();
+    console.log("🔥: A user disconnected");
+  });
+
 });
   http.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
