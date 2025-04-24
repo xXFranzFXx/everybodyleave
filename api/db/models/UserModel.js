72456bcf12922fd7197f06c3a289c5fcc93fdb0b
xXFranzFXx <franz.fernando@testpro.io>
2025-04-24 10:13:17 -0700

    Update UserModel.js

--- api/db/models/UserModel.js
+++ api/db/models/UserModel.js
@@ -2,9 +2,23 @@ const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
     _id: String,
-    email: String,
-    phone: String,
-    reminder: String
+    email: { 
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    phone: { 
+        type: String,
+        trim: true,
+        required: true,
+    },
+    reminder: String,
+    role: {
+        type: String,
+        default: 'subscriber'
+    },
 });
 
 module.exports = mongoose.model('Users', UserSchema);
