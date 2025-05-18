e718abc77015effff75526ba43086a47b8b7d4e8
xXFranzFXx <franz.fernando@testpro.io>
2025-05-18 07:53:22 -0700

    Update event.js

--- src/sockets/event.js
+++ src/sockets/event.js
@@ -9,4 +9,12 @@ export const socketEvents = ({ state }) => {
     socket.on('textBeeSms', ({ job }) => {
       return { ...state, job };
     });
+
+    socket.on('email otp verification', ({ otp_email }) => {
+      return { ...state, otp_email };
+    })
+
+    socket.on('sms otp verification', ({ otp_sms }) => {
+      return { ...state, otp_sms };
+    })
 }
