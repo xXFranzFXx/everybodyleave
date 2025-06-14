6112a879bc4b94b41a907cd0af8863d44e982707
xXFranzFXx <franz.fernando@testpro.io>
2025-06-13 21:30:27 -0700

    Update checkJwt.js

--- api/jwt/checkJwt.js
+++ api/jwt/checkJwt.js
@@ -8,10 +8,10 @@ const checkJwt = jwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+    jwksUri: `${process.env.AUTH0_DOMAIN}.well-known/jwks.json`,
   }),
   audience: process.env.AUTH0_AUDIENCE,
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  issuer: process.env.AUTH0_DOMAIN,
   algorithms: ["RS256"],
 });
 
