4d386ad56be648b41190d391528d14eb6acf2c35
xXFranzFXx <franz.fernando@testpro.io>
2025-06-23 14:28:53 -0700

    Update events.js

--- api/controllers/events.js
+++ api/controllers/events.js
@@ -1,13 +1,15 @@
 const Event = require('../models/EventModel');
 const User = require('../models/UserModel');
+const session = await mongoose.startSession();
 
 exports.saveReminder = (req, res) => {
      const { token, phone, reminderDate, timezone } = req.body;
         const decoded = jwtDecode(token);
         const { mongoId } = decoded
         const tz =  `timezones.` + timezone
-         
-        Event.findOneAndUpdate({ date: new Date(reminderDate), tz: { $ne: mongoId } }, // Find the document and check if the value exists in the nested array
+       try{
+        session.startTransaction();
+        const event = Event.findOneAndUpdate({ date: new Date(reminderDate), tz: { $ne: mongoId } }, // Find the document and check if the value exists in the nested array
                 { $addToSet: { tz: { mongoId } } }, // If not found, add the value to the nested array
                 { new: true }, // Return the updated document
                 function(err, doc) {
@@ -17,8 +19,23 @@ exports.saveReminder = (req, res) => {
                     } else {
                         res.status(200).json({ doc })
                     }
-            })
-        }
+            }, { session });
+
+        User.updateOne({_id: mongoId}, 
+            {
+                $set: { reminder: event._id}
+            }, { session });
+            
+            session.commitTransaction();
+            return event
+        } catch (error) {
+            session.abortTransaction();
+            throw error;
+    } finally {
+        session.endSession();
+    }
+
+    }
 
 //  { $pull: { 'preferences.notifications': notificationValue } }, // Use $pull to remove the value
  //     { new: true } // Return the updated document
