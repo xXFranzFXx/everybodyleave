1425086c6b72b6346b8a0dc6e05621fd9780e557
xXFranzFXx <franz.fernando@testpro.io>
2025-06-22 21:39:41 -0700

    Update events.js

--- api/controllers/events.js
+++ api/controllers/events.js
@@ -20,3 +20,24 @@ exports.saveReminder = (req, res) => {
             })
         }
 
+//  { $pull: { 'preferences.notifications': notificationValue } }, // Use $pull to remove the value
+ //     { new: true } // Return the updated document
+
+ exports.cancelReminder = (req, res) => {
+     const { token, phone, reminderDate, timezone } = req.body;
+        const decoded = jwtDecode(token);
+        const { mongoId } = decoded
+        const tz =  `timezones.` + timezone
+         
+        Event.findOneAndUpdate({ date: new Date(reminderDate)}, // Find the document and check if the value exists in the nested array
+                { $pull: { tz: { mongoId } } }, // If not found, add the value to the nested array
+                { new: true }, // Return the updated document
+                function(err, doc) {
+                     if (err) {
+                        console.log('Cannot cancel reminder', err);
+                        return res.status(401).json({error: 'Error cancelling reminder'});
+                    } else {
+                        res.status(200).json({ doc })
+                    }
+            })
+        }
