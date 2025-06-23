77835afdf3aa84aaf3836ecb3d7f5582ed0b878a
xXFranzFXx <franz.fernando@testpro.io>
2025-06-23 15:17:25 -0700

    Update events.js

--- api/controllers/events.js
+++ api/controllers/events.js
@@ -2,35 +2,33 @@ const Event = require('../models/EventModel');
 const User = require('../models/UserModel');
 const session = await mongoose.startSession();
 
-exports.saveReminder = (req, res) => {
+exports.saveReminder = async (req, res) => {
      const { token, phone, reminderDate, timezone } = req.body;
-        const decoded = jwtDecode(token);
-        const { mongoId } = decoded
-        const tz =  `timezones.` + timezone
-       try{
-        session.startTransaction();
-        const event = Event.findOneAndUpdate({ date: new Date(reminderDate), tz: { $ne: mongoId } }, // Find the document and check if the value exists in the nested array
-                { $addToSet: { tz: { mongoId } } }, // If not found, add the value to the nested array
-                { new: true }, // Return the updated document
-                function(err, doc) {
-                     if (err) {
-                        console.log('Cannot create scheduled reminder', err);
-                        return res.status(401).json({error: 'Error saving reminder'});
-                    } else {
-                        res.status(200).json({ doc })
-                    }
-            }, { session });
+        const decoded = await jwtDecode(token);
+        const { mongoId } = decoded;
+        const tz =  `timezones.` + timezone;
+       try {
+            session.startTransaction();
 
-        User.updateOne({_id: mongoId}, 
-            {
-                $set: { reminder: event._id }
-            }, { session });
-            
-            session.commitTransaction();
-            return event
+            const event = await Event.findOneAndUpdate({ date: new Date(reminderDate), tz: { $ne: mongoId } }, // Find the document and check if the value exists in the nested array
+                    { $addToSet: { tz: { mongoId } } }, // If not found, add the value to the nested array
+                    { new: true }, { session });
+
+                await User.updateOne({_id: mongoId}, 
+                    {
+                        $set: { reminder: event[0]._id }
+                    }, { session });
+                
+                await session.commitTransaction();
+
+                console.log("Transaction successful");
+                return res.status(200).json({ event });
+
         } catch (error) {
-            session.abortTransaction();
+            await session.abortTransaction();
+            res.status(401).json({error: 'Error saving reminder'});
             throw error;
+
     } finally {
         session.endSession();
     }
@@ -40,35 +38,35 @@ exports.saveReminder = (req, res) => {
 //  { $pull: { 'preferences.notifications': notificationValue } }, // Use $pull to remove the value
  //     { new: true } // Return the updated document
 
- exports.cancelReminder = (req, res) => {
+ exports.cancelReminder = async (req, res) => {
      const { token, phone, reminderDate, timezone } = req.body;
-        const decoded = jwtDecode(token);
-        const { mongoId } = decoded
-        const tz =  `timezones.` + timezone
+        const decoded = await jwtDecode(token);
+        const { mongoId } = decoded;
+        const tz =  `timezones.` + timezone;
         try {
             session.startTransaction();
         
-            const event = Event.findOneAndUpdate({ date: new Date(reminderDate)}, // Find the document and check if the value exists in the nested array
+            const event = await Event.findOneAndUpdate({ date: new Date(reminderDate)}, // Find the document and check if the value exists in the nested array
                 { $pull: { tz: { mongoId } } }, // If not found, add the value to the nested array
-                { new: true }, // Return the updated document
-                function(err, doc) {
-                     if (err) {
-                        console.log('Cannot cancel reminder', err);
-                        return res.status(401).json({error: 'Error cancelling reminder'});
-                    } else {
-                        res.status(200).json({ doc })
-                    }
-            }, {session})
-         User.updateOne({ id: mongoId }, {
-            $pull: {
-                'reminder': event._id
-            } 
-         }, {session})
-           session.commitTransaction();
-            return event
+                { new: true }, {session}); // Return the updated document
+       
+                await User.updateOne({ id: mongoId }, {
+                 $pull: {
+                     'reminder': event[0]._id
+                    } 
+                }, {session} );
+
+             await session.commitTransaction();
+             
+             console.log("Transaction successful");
+             return res.status(200).json({ event });
+
         } catch (error) {
-            session.abortTransaction();
+            await session.abortTransaction();
+            console.log("Transaction failed: ", error);
+            res.status(401).json({error: 'Error cancelling reminder'});
             throw error;
+
     } finally {
         session.endSession();
     }
