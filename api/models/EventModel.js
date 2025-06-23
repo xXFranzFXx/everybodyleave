6b230250e0ca4ff3018fbfd4e913c4cacda0ed23
xXFranzFXx <franz.fernando@testpro.io>
2025-06-22 21:07:32 -0700

    Update EventModel.js

--- api/models/EventModel.js
+++ api/models/EventModel.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
-// const User = require('./UserModel');
+
 
 const EventSchema = mongoose.Schema({
     _id: String,
     date: Date,
-    users: [{type: Schema.Types.ObjectId, ref: 'User'}]
+    timezones: {
+       type: Map,
+       of: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
+    }
+
 });
 
 module.exports = mongoose.model('Event',EventSchema);
