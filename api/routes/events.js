be508131c89b47c54756d86f2011de1559692fa6
xXFranzFXx <franz.fernando@testpro.io>
2025-06-24 07:43:06 -0700

    Create events.js

---
+++ api/routes/events.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { checkJwt } = require('../jwt/checkJwt')
+const { saveReminder, cancelReminder } = require('../controllers/events')
+
+router.post('/events/save', saveReminder);
+router.put('/events/cancel', checkJwt, cancelReminder);
+
+module.exports = { router };
