38d97591bf16a727eb1c47dc9bdcff4ab34e7d95
xXFranzFXx <franz.fernando@testpro.io>
2025-06-29 21:32:01 -0700

    Update user.js

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // import controller
-const { postSignIn, update } = require('../controllers/user');
+const { postSignIn, scheduleReminder } = require('../controllers/user');
+const { checkJwt } =  require('../jwt/checkJwt');
+
+router.get('/user/:id', checkJwt, postSignIn);
+router.put('/user/updateUser', checkJwt, scheduleReminder);
+
+module.exports = router ;
 
 
-module.exports = router;
+
