f2e086b39f7631d200040ad0db9f041fbf0158e9
xXFranzFXx <franz.fernando@testpro.io>
2025-08-21 10:31:08 -0700

    configuration file

--- src/form/Reminders.jsx
+++ src/form/Reminders.jsx
@@ -3,138 +3,129 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { FormInputDropdown } from '../form-components/FormInputDropdown';
 import { useSocketContext } from '../context/SocketProvider';
 import { FormProvider, Controller } from 'react-hook-form';
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
 import axios from 'axios';
-import { subscribe, useSnapshot } from 'valtio'
-
+import { subscribe, useSnapshot } from 'valtio';
 
 import { TextField, FormControlLabel, Typography, Checkbox, Button, Grid2, Box, Paper } from '@mui/material';
 export const Reminders = () => {
-    const { state } = useSocketContext();
-    const { snap } = useSnapshot(state);
-    const { phone, timezone } = state;
-    const { user, getAccessTokenSilently } = useAuth0();
-    const { reminderDate, mongoId } = user;
-    const [ displayedDate, setDisplayedDate] = useState();
-    // const methods = useForm({ defaultValues: defaultValues || ""});
-    // const {  handleSubmit, register,  getValues, reset, control, setValue, formState: {errors} } = methods;
-    const isBeforeNow = (date) =>  {
+  const { state } = useSocketContext();
+  const { snap } = useSnapshot(state);
+  const { phone, timezone } = state;
+  const { user, getAccessTokenSilently } = useAuth0();
+  const { reminderDate, mongoId } = user;
+  const [displayedDate, setDisplayedDate] = useState();
+  // const methods = useForm({ defaultValues: defaultValues || ""});
+  // const {  handleSubmit, register,  getValues, reset, control, setValue, formState: {errors} } = methods;
+  const isBeforeNow = (date) => {
     return new Date(date) < new Date();
-  }
-    const onDelete = async () => {
-     const token = await getAccessTokenSilently();
-     
-     console.log("mongoId: ", mongoId)
-     try {
-             const response = await axios({
-                    method: 'PUT',
-                    url: `http://localhost:4000/api/events/remove`,
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    },
-                    data:{
-                      mongoId: mongoId,
-                      phone: phone,
-                      datetime: new Date(reminderDate), 
-                      timezone: timezone
-                    }
-                })
-                const res = await response.data;
-                setDisplayedDate("")
-                return res;
-              } catch (err) {
-                console.log("Error cancelling reminder: ", err)
-              }
-          
-    };
-  
-    const onEdit = () => {
+  };
+  const onDelete = async () => {
+    const token = await getAccessTokenSilently();
 
-    };
-      useEffect(
+    console.log('mongoId: ', mongoId);
+    try {
+      const response = await axios({
+        method: 'PUT',
+        url: `http://localhost:4000/api/events/remove`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        data: {
+          mongoId: mongoId,
+          phone: phone,
+          datetime: new Date(reminderDate),
+          timezone: timezone,
+        },
+      });
+      const res = await response.data;
+      setDisplayedDate('');
+      return res;
+    } catch (err) {
+      console.log('Error cancelling reminder: ', err);
+    }
+  };
+
+  const onEdit = () => {};
+  useEffect(
     () =>
       subscribe(state, () => {
-        const callback = () =>{
-        if (state.scheduledReminder) {
-          // conditionally update local state
-          // let newDate = [...displayedDate, state.reminder]
-          setDisplayedDate(state.reminder)
-        }
-      }
-      const unsubscribe = subscribe(state, callback)
-      callback()
-      return unsubscribe
+        const callback = () => {
+          if (state.scheduledReminder) {
+            // conditionally update local state
+            // let newDate = [...displayedDate, state.reminder]
+            setDisplayedDate(state.reminder);
+          }
+        };
+        const unsubscribe = subscribe(state, callback);
+        callback();
+        return unsubscribe;
       }),
-    [],
-  )
-    useEffect (() => {
-      if(reminderDate)
-      setDisplayedDate(reminderDate);
-    },[reminderDate])
-    return (
-        <>
-        {  displayedDate  && 
-                // <Paper
-                //     elevation={0}
-                //     style={{
-                //         display: "grid",
-                //         flexWrap: "wrap",
-                //         gridRowGap: "20px",
-                //         padding: "20px",
-                //         margin: "auto",
-                //         width: "auto",
-                //         maxWidth: "620px",
-                //         minWidth: "250px",
-                //         marginTop: "30px"
-                //     }} 
-                // >
-                 <Box px={1} py={2} sx={{width: '45%' }}>   
-                    <Grid2 container  spacing={{ xs: 2, md: 3 }} columnSpacing={{ xs: 12, sm: 10, md: 3 }}>
-                    <Grid2 item size={12} sx={{width: 'auto'}}>
-                   <Typography variant="h7"  >
-                   { isBeforeNow(displayedDate) ? 'Past Reminders' : 'Upcoming Reminders' }
-                  </Typography>
-                                      </Grid2>
+    []
+  );
+  useEffect(() => {
+    if (reminderDate) setDisplayedDate(reminderDate);
+  }, [reminderDate]);
+  return (
+    <>
+      {
+        displayedDate && (
+          // <Paper
+          //     elevation={0}
+          //     style={{
+          //         display: "grid",
+          //         flexWrap: "wrap",
+          //         gridRowGap: "20px",
+          //         padding: "20px",
+          //         margin: "auto",
+          //         width: "auto",
+          //         maxWidth: "620px",
+          //         minWidth: "250px",
+          //         marginTop: "30px"
+          //     }}
+          // >
+          <Box px={1} py={2} sx={{ width: '45%' }}>
+            <Grid2 container spacing={{ xs: 2, md: 3 }} columnSpacing={{ xs: 12, sm: 10, md: 3 }}>
+              <Grid2 item size={12} sx={{ width: 'auto' }}>
+                <Typography variant="h7">
+                  {isBeforeNow(displayedDate) ? 'Past Reminders' : 'Upcoming Reminders'}
+                </Typography>
+              </Grid2>
 
-                                  <Grid2 item size={3}>
-
-                  <Typography variant="h8" align="center" margin="dense">
-                  { displayedDate ? 
-                   (new Date(displayedDate).toLocaleDateString('en-EN', { weekday: 'long' })+' '+new Date(displayedDate).toLocaleDateString() +' ' +'@' + new Date(reminderDate).toLocaleTimeString())
-                   :
-                   typeof state.reminder != ''  ?  state.reminder
-                  //  (new Date(state.reminder[0]).toLocaleDateString('en-EN', { weekday: 'long' })+' '+new Date(state.reminder[0]).toLocaleDateString() +' ' +'@' + new Date(state.reminder[0]).toLocaleTimeString())
-                   :
-                   `There are no reminders.`
-                  }
-                  </Typography>
-                  </Grid2>
-                  </Grid2>    
-                  { displayedDate && !isBeforeNow(displayedDate) &&
-                  <Box mt={3}>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={onDelete}
-                    >
-                      Delete
-                    </Button>        
-                     {/* <Button
+              <Grid2 item size={3}>
+                <Typography variant="h8" align="center" margin="dense">
+                  {displayedDate
+                    ? new Date(displayedDate).toLocaleDateString('en-EN', { weekday: 'long' }) +
+                      ' ' +
+                      new Date(displayedDate).toLocaleDateString() +
+                      ' ' +
+                      '@' +
+                      new Date(reminderDate).toLocaleTimeString()
+                    : typeof state.reminder != ''
+                    ? state.reminder
+                    : //  (new Date(state.reminder[0]).toLocaleDateString('en-EN', { weekday: 'long' })+' '+new Date(state.reminder[0]).toLocaleDateString() +' ' +'@' + new Date(state.reminder[0]).toLocaleTimeString())
+                      `There are no reminders.`}
+                </Typography>
+              </Grid2>
+            </Grid2>
+            {displayedDate && !isBeforeNow(displayedDate) && (
+              <Box mt={3}>
+                <Button variant="contained" color="primary" onClick={onDelete}>
+                  Delete
+                </Button>
+                {/* <Button
                       variant="contained"
                       color="primary"
                       onClick={onEdit}
                     >
                       Edit
                     </Button>     */}
-                   
-                  </Box>     
-}
-                </Box>     
-              // </Paper>   
-}
-              </>  
-       
-    )
-
-} 
-
+              </Box>
+            )}
+          </Box>
+        )
+        // </Paper>
+      }
+    </>
+  );
+};
--- src/form/SimpleForm.jsx
+++ src/form/SimpleForm.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useCallback, useState, useContext, useRef } from 'react';
 import { useSocketContext } from '../context/SocketProvider';
-import { FormProvider,  useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { useAuth0 } from '@auth0/auth0-react';
 import { FormInputTel } from '../form-components/FormInputTel';
 import { FormInputText } from '../form-components/FormInputText';
@@ -14,157 +14,159 @@ import { TextField, FormControlLabel, Typography, Checkbox, Button, Grid2, Box, 
 import { Reminders } from './Reminders';
 import axios from 'axios';
 const SimpleForm = () => {
-    const { state } = useSocketContext();
-    const { type, phone, acceptTerms, default_timezone, otp, timezone, reminder, scheduledReminder } = state;
-    const defaultValues = {
-        datetime:"",
-        phone: phone || "",
-        acceptTerms:"",
-        utcdate: "",
-        timezone:"",
-        message:"",
-    }
-    const { user, logout, getAccessTokenSilently } = useAuth0();
-    const { role, reminderDate, mongoId } = user;
-    const { saveUserReminder } = useContext(MetadataContext);
-    const methods = useForm({ defaultValues: defaultValues || ""});
-    const {  handleSubmit, register,  getValues, reset, control, setValue, formState: {errors} } = methods;
-    const [error, setError] = useState(false);
-     const setReminder = useCallback(() => {
-      state.scheduledReminder = true;
-    },[])
-    
-    const saveReminder = async (datetime, phone, timezone) => {
-     const token = await getAccessTokenSilently();
-     console.log("mongoId: ", mongoId)
-     try {
-             const response = await   axios({
-                    method: 'POST',
-                    url: `http://localhost:4000/api/events/save`,
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    },
-                    data:{
-                      mongoId: mongoId,
-                      phone: phone,
-                      datetime: new Date(datetime), 
-                      timezone: timezone
-                    }
-                })
-                const res = await response.data;
-                saveUserReminder(res.date)
-                state.reminder = res.date
-                setReminder();
-                return res;
-              } catch (err) {
-                console.log("Error saving reminder: ", err)
-              }
-          }
+  const { state } = useSocketContext();
+  const { type, phone, acceptTerms, default_timezone, otp, timezone, reminder, scheduledReminder } = state;
+  const defaultValues = {
+    datetime: '',
+    phone: phone || '',
+    acceptTerms: '',
+    utcdate: '',
+    timezone: '',
+    message: '',
+  };
+  const { user, logout, getAccessTokenSilently } = useAuth0();
+  const { role, reminderDate, mongoId } = user;
+  const { saveUserReminder } = useContext(MetadataContext);
+  const methods = useForm({ defaultValues: defaultValues || '' });
+  const {
+    handleSubmit,
+    register,
+    getValues,
+    reset,
+    control,
+    setValue,
+    formState: { errors },
+  } = methods;
+  const [error, setError] = useState(false);
+  const setReminder = useCallback(() => {
+    state.scheduledReminder = true;
+  }, []);
 
-   
-    const onSubmit = (data) => {
-        const {datetime, message} = data;
-        const zeroSeconds = new Date(datetime).setMilliseconds(0);
-        const date = new Date(datetime);
-        console.log(date.getTime())
-        console.log(date.toISOString())
-        state['utcdate'] = date.toUTCString();
-        data.utcdate = date.toUTCString();
-        // const reminder = {
-        //   event: datetime,
-        //   message: message
-        // }
-        data.timezone = timezone
-        saveReminder(zeroSeconds, phone, timezone)
-        // textBeeSms(data)
-        // logout();
-        // cronTextBeeSms(data)
-        // textBeeSms(data);
+  const saveReminder = async (datetime, phone, timezone) => {
+    const token = await getAccessTokenSilently();
+    console.log('mongoId: ', mongoId);
+    try {
+      const response = await axios({
+        method: 'POST',
+        url: `http://localhost:4000/api/events/save`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        data: {
+          mongoId: mongoId,
+          phone: phone,
+          datetime: new Date(datetime),
+          timezone: timezone,
+        },
+      });
+      const res = await response.data;
+      saveUserReminder(res.date);
+      state.reminder = res.date;
+      setReminder();
+      return res;
+    } catch (err) {
+      console.log('Error saving reminder: ', err);
     }
-    const handleChange = () => {}
-//  useEffect(() => {
-// console.log(getValues("reminder"))
-//  },[getValues("reminder")])
-    return (
-      <>        
+  };
+
+  const onSubmit = (data) => {
+    const { datetime, message } = data;
+    const zeroSeconds = new Date(datetime).setMilliseconds(0);
+    const date = new Date(datetime);
+    console.log(date.getTime());
+    console.log(date.toISOString());
+    state['utcdate'] = date.toUTCString();
+    data.utcdate = date.toUTCString();
+    // const reminder = {
+    //   event: datetime,
+    //   message: message
+    // }
+    data.timezone = timezone;
+    saveReminder(zeroSeconds, phone, timezone);
+    // textBeeSms(data)
+    // logout();
+    // cronTextBeeSms(data)
+    // textBeeSms(data);
+  };
+  const handleChange = () => {};
+  //  useEffect(() => {
+  // console.log(getValues("reminder"))
+  //  },[getValues("reminder")])
+  return (
+    <>
       <FormProvider {...methods}>
         <Paper
-            elevation={24}
-            style={{
-                display: "flex",
-                flexWrap: "wrap",
-                alignItems: "stretch",
-                justifyItems: "stretch",
-                flexDirection: "row",
-                justifyContent: "space-evenly",
-                gridRowGap: "20px",
-                padding: "20px",
-                margin: "auto",
-                width: "auto",
-                maxWidth: "620px",
-                minWidth: "250px",
-                marginTop: "30px",
-                borderRadius: "5px",
-                border: "1px solid gray"
-            
-            }} 
+          elevation={24}
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignItems: 'stretch',
+            justifyItems: 'stretch',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            gridRowGap: '20px',
+            padding: '20px',
+            margin: 'auto',
+            width: 'auto',
+            maxWidth: '620px',
+            minWidth: '250px',
+            marginTop: '30px',
+            borderRadius: '5px',
+            border: '1px solid gray',
+          }}
         >
-           <Typography variant="h6" align="center" margin="dense">
+          <Typography variant="h6" align="center" margin="dense">
             Everybodyleave Weekly Reminders
           </Typography>
 
-        <Box px={3} py={2}  sx={{border: '2px solid black', borderRadius: '5px', width: '45%'}}>        
-                   <Grid2 container spacing={{ xs: 2, md: 3 }} columnSpacing={{ xs: 12, sm: 10, md: 3 }}>
+          <Box px={3} py={2} sx={{ border: '2px solid black', borderRadius: '5px', width: '45%' }}>
+            <Grid2 container spacing={{ xs: 2, md: 3 }} columnSpacing={{ xs: 12, sm: 10, md: 3 }}>
+              <Typography variant="h7" align="center" margin="dense">
+                Set Reminder
+              </Typography>
+              <Grid2 size={12} sx={{ width: '100%' }}>
+                {user && <FormInputTel name="phone" control={control} label="Your Phone" />}
+              </Grid2>
+              <Grid2 size={12}>
+                <FormInputDateTime name="datetime" control={control} label="Date/Time*" />
+              </Grid2>
 
-         <Typography variant="h7" align="center" margin="dense">
-           Set Reminder
-          </Typography>
-          <Grid2 size={12} sx={{width: '100%'}}>
-            { user &&          
-              <FormInputTel name="phone" control={control} label="Your Phone" />
-            }
-          </Grid2>
-          <Grid2 size={12}>
-            <FormInputDateTime name="datetime" control={control} label="Date/Time*" />
-          </Grid2>
-          
-          {
-            role === 'basic' && (new Date(reminderDate) > new Date() && scheduledReminder) ? 
-         ( <Grid2 item xs={12} sm={4} style={{paddingTop: 15}} >
-            <Typography variant="h6" align="center" margin="dense">
-                Upgrade now to schedule multiple reminders
-          </Typography>          </Grid2>  )
-          :
-
-          (<Grid2 item mt={3} size={12}>
-            <Button
-            disabled={(role === 'basic' && (new Date(reminderDate) > new Date()) && scheduledReminder)? true: false}
-              variant="contained"
-              color="primary"
-              onClick={handleSubmit(onSubmit)}
-              sx={{width: '100%'}}
-            >
-              Save
-            </Button>        
-          </Grid2>  )   
-}
-</Grid2>
-        </Box>  
-        <Box px={3} py={2}  sx={{border: '2px solid black', borderRadius: '5px', width: '45%'}}>   
-        <Grid2 container>
-        <Grid2 item>
-        <Reminders/>
-        </Grid2>
-        </Grid2>
-        </Box>
-      </Paper>   
-        
-        </FormProvider>
-        {/* {state.scheduledReminder &&
+              {role === 'basic' && new Date(reminderDate) > new Date() && scheduledReminder ? (
+                <Grid2 item xs={12} sm={4} style={{ paddingTop: 15 }}>
+                  <Typography variant="h6" align="center" margin="dense">
+                    Upgrade now to schedule multiple reminders
+                  </Typography>
+                </Grid2>
+              ) : (
+                <Grid2 item mt={3} size={12}>
+                  <Button
+                    disabled={
+                      role === 'basic' && new Date(reminderDate) > new Date() && scheduledReminder ? true : false
+                    }
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSubmit(onSubmit)}
+                    sx={{ width: '100%' }}
+                  >
+                    Save
+                  </Button>
+                </Grid2>
+              )}
+            </Grid2>
+          </Box>
+          <Box px={3} py={2} sx={{ border: '2px solid black', borderRadius: '5px', width: '45%' }}>
+            <Grid2 container>
+              <Grid2 item>
+                <Reminders />
+              </Grid2>
+            </Grid2>
+          </Box>
+        </Paper>
+      </FormProvider>
+      {/* {state.scheduledReminder &&
         <Reminders/>
 } */}
-        </>
-
-    )
-}
+    </>
+  );
+};
 export default SimpleForm;
