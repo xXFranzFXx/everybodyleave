577cadb5b3eeddf5fb9b9c153852cc96df517b7a
xXFranzFXx <franz.fernando@testpro.io>
2025-08-24 12:37:09 -0700

    Update Reminders.jsx

--- src/form/Reminders.jsx
+++ src/form/Reminders.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import dayjs from 'dayjs';
+import calendar from 'dayjs/plugin/calendar';
 import { FormInputDropdown } from '../form-components/FormInputDropdown';
 import { useSocketContext } from '../context/SocketProvider';
 import { FormProvider, Controller } from 'react-hook-form';
@@ -8,13 +10,26 @@ import axios from 'axios';
 import { subscribe, useSnapshot } from 'valtio';
 
 import { TextField, FormControlLabel, Typography, Checkbox, Button, Grid2, Box, Paper } from '@mui/material';
+
 export const Reminders = () => {
+  dayjs.extend(calendar);
+  const currentDate = dayjs();
+  const customFormat = {
+    sameDay: '[Today at] h:mm A',
+    nextDay: '[Tomorrow at] h:mm A',
+    nextWeek: '[Next] dddd [at] h:mm A',
+    lastDay: '[Yesterday at] h:mm A',
+    lastWeek: '[Last] dddd [at] h:mm A',
+    sameElse: 'DD[th of] MMMM YYYY [at] h:mm A',
+  };
   const { state } = useSocketContext();
   const { snap } = useSnapshot(state);
   const { phone, timezone } = state;
   const { user, getAccessTokenSilently } = useAuth0();
   const { reminderDate, mongoId } = user;
   const [displayedDate, setDisplayedDate] = useState();
+  const [pastReminders, setPastReminders] = useState([]);
+  const [upcomingReminders, setUpcomingReminders] = useState([]);
   // const methods = useForm({ defaultValues: defaultValues || ""});
   // const {  handleSubmit, register,  getValues, reset, control, setValue, formState: {errors} } = methods;
   const isBeforeNow = (date) => {
@@ -64,68 +79,44 @@ export const Reminders = () => {
     []
   );
   useEffect(() => {
-    if (reminderDate) setDisplayedDate(reminderDate);
+    let reminders = [];
+    if (Array.isArray(reminderDate) && reminderDate.length) {
+      reminders = reminderDate.map(({ eventDate }) => eventDate);
+      setDisplayedDate(reminders);
+    }
   }, [reminderDate]);
   return (
     <>
-      {
-        displayedDate && (
-          // <Paper
-          //     elevation={0}
-          //     style={{
-          //         display: "grid",
-          //         flexWrap: "wrap",
-          //         gridRowGap: "20px",
-          //         padding: "20px",
-          //         margin: "auto",
-          //         width: "auto",
-          //         maxWidth: "620px",
-          //         minWidth: "250px",
-          //         marginTop: "30px"
-          //     }}
-          // >
-          <Box px={1} py={2} sx={{ width: '45%' }}>
-            <Grid2 container spacing={{ xs: 2, md: 3 }} columnSpacing={{ xs: 12, sm: 10, md: 3 }}>
-              <Grid2 item size={12} sx={{ width: 'auto' }}>
-                <Typography variant="h7">
-                  {isBeforeNow(displayedDate) ? 'Past Reminders' : 'Upcoming Reminders'}
-                </Typography>
-              </Grid2>
-
-              <Grid2 item size={3}>
-                <Typography variant="h8" align="center" margin="dense">
-                  {displayedDate
-                    ? new Date(displayedDate).toLocaleDateString('en-EN', { weekday: 'long' }) +
-                      ' ' +
-                      new Date(displayedDate).toLocaleDateString() +
-                      ' ' +
-                      '@' +
-                      new Date(reminderDate).toLocaleTimeString()
-                    : typeof state.reminder != ''
-                    ? state.reminder
-                    : //  (new Date(state.reminder[0]).toLocaleDateString('en-EN', { weekday: 'long' })+' '+new Date(state.reminder[0]).toLocaleDateString() +' ' +'@' + new Date(state.reminder[0]).toLocaleTimeString())
-                      `There are no reminders.`}
-                </Typography>
-              </Grid2>
-            </Grid2>
-            {displayedDate && !isBeforeNow(displayedDate) && (
-              <Box mt={3}>
-                <Button variant="contained" color="primary" onClick={onDelete}>
-                  Delete
-                </Button>
-                {/* <Button
+      <Box px={1} py={2} sx={{ width: '45%' }}>
+        <Typography variant="h8">Scheduled Reminders</Typography>
+        {displayedDate &&
+          displayedDate.map((date, idx) => (
+            <>
+              <Grid2 container spacing={{ xs: 2, md: 3 }}>
+                <Grid2 key={idx} item size={12} sx={{ width: 'auto' }}>
+                  <Typography variant="h7">
+                    {/* {isBeforeNow(date) ? 'Past Reminders' : 'Upcoming Reminders'} */}
+                    {dayjs(date).calendar(currentDate, customFormat)}
+                  </Typography>
+                </Grid2>
+                {displayedDate && !isBeforeNow(date) && (
+                  <Box mt={3}>
+                    <Button variant="contained" color="primary" onClick={onDelete}>
+                      Delete
+                    </Button>
+                    {/* <Button
                       variant="contained"
                       color="primary"
                       onClick={onEdit}
                     >
                       Edit
                     </Button>     */}
-              </Box>
-            )}
-          </Box>
-        )
-        // </Paper>
-      }
+                  </Box>
+                )}
+              </Grid2>
+            </>
+          ))}
+      </Box>
     </>
   );
 };
