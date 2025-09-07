aebb667bf2a2d4d5a1c637e177f878740e3d1cb3
xXFranzFXx <franz.fernando@testpro.io>
2025-09-07 15:54:30 -0700

    Update Auth0ProviderWithHistory.jsx

--- src/context/Auth0ProviderWithHistory.jsx
+++ src/context/Auth0ProviderWithHistory.jsx
@@ -6,8 +6,8 @@ const Auth0ProviderWithHistory = ({ children }) => {
        domain={process.env.REACT_APP_AUTH0_DOMAIN}
        clientId={process.env.REACT_APP_AUTH0_CLIENTID}
        authorizationParams={{
-        // redirect_uri: window.location.origin,
-        redirect_uri: `https://everybodyleave-2.onrender.com`,
+        redirect_uri: window.location.origin,
+        // redirect_uri: `https://everybodyleave-2.onrender.com`,
         audience: `${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`,
         scope: "openid profile read:current_user update:current_user_metadata"
     }}
