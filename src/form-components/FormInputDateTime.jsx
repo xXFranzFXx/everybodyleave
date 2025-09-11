78c191f57576de836e4b29e2ca003413e8d31f30
xXFranzFXx <franz.fernando@testpro.io>
2025-09-11 15:58:19 -0700

    Update FormInputDateTime.jsx

--- src/form-components/FormInputDateTime.jsx
+++ src/form-components/FormInputDateTime.jsx
@@ -92,7 +92,7 @@ export const FormInputDateTime = ({ name, control, label }) => {
       scheduled = reminders.map((result) => dayjs(result).date());
       console.log('scheduled: ', scheduled);
       return (
-        weekDayArr.includes(weekday) && (reminders.includes(`${pickerDate}`) || scheduled.includes(dayDate) || !dateSet.has(dayDate)) 
+        (weekDayArr.includes(weekday) || reminders.includes(`${pickerDate}`)) || (scheduled.includes(dayDate) || !dateSet.has(dayDate)) 
         
       );
     } else {
