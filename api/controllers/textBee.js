2c2ac775e4ee1c4ef5177e5338fb81a84840c564
xXFranzFXx <franz.fernando@testpro.io>
2025-09-29 16:58:27 -0700

    Update textBee.js

--- api/controllers/textBee.js
+++ api/controllers/textBee.js
@@ -18,6 +18,21 @@ exports.textBeeSms = async (req, res) => {
     return result;
 }
 
+exports.textBeeBulkSms = async (req, res) => {
+    const { phone, dateScheduled } = req.body; 
+    const message = `This is your scheduled reminder from EbL. Please reply with 1 if you are still attending the event. Respond with 2 if you aren't.`
+    const response = await axios.post(`${BASE_URL}/gateway/devices/${DEVICE_ID}/send-sms`, {
+    recipients: [phone],
+        message: message,
+        }, {
+        headers: {
+            'x-api-key': API_KEY
+        }
+    });
+    const result = await response.data;
+    return result;
+}
+
 exports.textBeeSmsCancel = async (req, res) => {
     const { phone, dateScheduled } = req.body; 
     const message = `You have cancelled your EbL reminder for ${dateScheduled}.`
