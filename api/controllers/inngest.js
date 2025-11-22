63c99e6a1f1d1449c6f710c079a96ddc4a1cf0c2
xXFranzFXx <franz.fernando@testpro.io>
2025-11-22 11:20:37 -0800

    Update inngest.js

--- api/controllers/inngest.js
+++ api/controllers/inngest.js
@@ -1,12 +1,29 @@
 const { inngest } = require('../inngest/reminders');
 
 exports.cancelLeave = async (req, res, next) => {
+    const { mongoId, phone, datetime } = req.body;
+
     await inngest.send({
     name: "reminders/delete.leave",
     data: {
-      mongoId: req.body.mongoId,
-      eventId: req.body.eventId
+      mongoId,
+      datetime,
+      phone,
     },
   }).catch(err => next(err));
   res.json({ message: 'Leave cancelled' });
-}
+};
+
+exports.createLeave = async (req, res, next) => {
+    const { mongoId, phone, datetime, timezone } = req.body;
+    await inngest.send({
+        name: "reminders/create.leave",
+        data: {
+            mongoId,
+            phone,
+            datetime,
+            timezone
+        },
+    }).catch(err => next(err));
+  res.json({ message: 'Leave created' });
+};
