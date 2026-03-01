e960e68178306e19d9556383709d4f7ee27f1395
xXFranzFXx <franz.fernando@testpro.io>
2026-02-28 19:52:20 -0800

    Update inngest.js

--- api/controllers/inngest.js
+++ api/controllers/inngest.js
@@ -25,7 +25,10 @@ exports.cancelLeave = async (req, res) => {
 
 exports.createLeave = async (req, res) => {
   const { mongoId, phone, dateScheduled, datetime, timezone, profileName, intention, logins, nudgeTimeUtc } = req.body;
- 
+  const userId = new mongoose.Types.ObjectId(`${mongoId}`);
+  const signedUpEvent = await SignedUpEvent.getSignedUpEventByDate(nudgeTimeUtc, userId);
+  console.log('signedUpEvent: ', signedUpEvent);
+  const eventId = new mongoose.Types.ObjectId(`${signedUpEvent._id}`);
   console.log('original datetime: ', dateScheduled);
  
   const nudgeReminderTs = await getNudgeReminders(nudgeTimeUtc, timezone);
@@ -42,7 +45,7 @@ exports.createLeave = async (req, res) => {
         profileName,
         logins,
         intention,
-        mongoId,
+        eventId,
         nudgeTimeUtc,
         nudgeReminderTs,
         nudgeMessage,
