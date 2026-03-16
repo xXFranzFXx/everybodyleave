b64c2c67d0d0b42375a7075f13d7fc9a49f6f36c
xXFranzFXx <franz.fernando@testpro.io>
2026-03-16 12:33:29 -0700

    Update paypal.js

--- api/routes/paypal.js
+++ api/routes/paypal.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { checkJwt } = require('../jwt/checkJwt');
-const { createOrder, captureOrder } = require('../controllers/paypal');
+const { createPaypalOrder, capturePaypalOrder } = require('../controllers/paypal');
 
-router.post('/payments/createOrder', checkJwt, createOrder);
-router.post('/payments/captureOrder', checkJwt, captureOrder);
+router.post('/payments/createOrder', checkJwt, createPaypalOrder);
+router.get('/payments/captureOrder', checkJwt, capturePaypalOrder);
+// router.post('/payments/createOrder',  createOrder);
+// router.post('/payments/captureOrder',  captureOrder);
 
 module.exports = router;
